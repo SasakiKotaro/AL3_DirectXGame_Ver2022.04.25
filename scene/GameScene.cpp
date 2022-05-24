876cf9829230ea585d9e5a0c1923e773d6a4ddd3
SasakiKotaro <k019g1380@g.neec.ac.jp>
2022-05-24 15:32:40 +0900

    02_03_ex課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -3,9 +3,12 @@
 #include <cassert>
 #include "AxisIndicator.h"
 #include "PrimitiveDrawer.h"
+#include <random>
 
 #define PI 3.141592
 #define TORADIAN(r) (r*PI/180)
+
+using namespace std;
 
 GameScene::GameScene() {}
 
@@ -22,12 +25,41 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
 
+	//乱数の設定
+	random_device seed_gen;
+	mt19937_64 engine(seed_gen());
+	uniform_real_distribution<float> rotRand(0, 359);
+	uniform_real_distribution<float> posRand(-10, 10);
+
 	//ファイル名を指定し、テクスチャ読み込み
 	textureHandle_ = TextureManager::Load("mario.jpg");
 	//3Dモデルの生成
 	model_ = Model::Create();
 
-	worldTransform_.Initialize();
+	for (WorldTransform& worldTransform : worldTransforms_)
+	{
+		worldTransform.Initialize();
+		//------------------------------------------------------------------------
+		//拡縮
+		Matrix4 scaleMat;
+		scaleMat.SetScale(1.0f, 1.0f, 1.0f);
+		//回転
+		Matrix4 rotaMat;
+		rotaMat.SetRotate(TORADIAN(rotRand(engine)), TORADIAN(rotRand(engine)), TORADIAN(rotRand(engine)));
+		//平行移動
+		Matrix4 transMat = MathUtility::Matrix4Identity();
+		transMat.SetTranslate(posRand(engine), posRand(engine), posRand(engine));
+
+		//掛け合わせ
+		worldTransform.matWorld_.SetMultiple(scaleMat, rotaMat, transMat);
+
+		worldTransform.TransferMatrix();				//更新
+		//------------------------------------------------------------------------
+	}
+
+	viewProjection_.eye = { 0,0,-50 };
+	viewProjection_.target = { 10,0,0 };
+	viewProjection_.up = { cosf(PI / 4.0f),sinf(PI / 4.0f),0.0f };
 	viewProjection_.Initialize();
 	debugCamera_ = new DebugCamera(WinApp::kWindowWidth, WinApp::kWindowHeight);
 
@@ -35,31 +67,61 @@ void GameScene::Initialize() {
 	AxisIndicator::GetInstance()->SetVisible(true);
 	//軸方向表示が参照するビュープロジェクションを指定する
 	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
-
 	//ラインが参照するビュープロジェクションを指定する
-	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
-
-	//------------------------------------------------------------------------
-	//拡縮
-	Matrix4 scaleMat;
-	scaleMat.SetScale(5.0f, 5.0f, 5.0f);
-	//回転
-	Matrix4 rotaMat;
-	rotaMat.SetRotate(TORADIAN(45.0f), TORADIAN(45.0f), TORADIAN(45.0f));
-	//平行移動
-	Matrix4 transMat = MathUtility::Matrix4Identity();
-	transMat.SetTranslate(10.f, 10.f, 10.f);
-
-	//掛け合わせ
-	worldTransform_.matWorld_.SetMultiple(scaleMat, rotaMat, transMat);
-	//------------------------------------------------------------------------
-
-	worldTransform_.TransferMatrix();				//更新
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
 }
 
 void GameScene::Update()
 {
-	debugCamera_->Update();
+	//視点の移動ベクトル
+	Vector3 move;
+
+	//視点移動の速さ
+	const float kEyeSpeed = 0.2f;
+	const float kTargetSpeed = 0.2f;
+	const float kUpRotSpeed = 0.05f;
+
+	//押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_W))
+	{
+		move.z += kEyeSpeed;
+	}
+	else if (input_->PushKey(DIK_S))
+	{
+		move.z -= kEyeSpeed;
+	}
+	if (input_->PushKey(DIK_LEFT))
+	{
+		move.x -= kTargetSpeed;
+	}
+	else if (input_->PushKey(DIK_RIGHT))
+	{
+		move.x += kTargetSpeed;
+	}
+	if (input_->PushKey(DIK_SPACE))
+	{
+		viewAngle += kUpRotSpeed;
+		viewAngle = fmodf(viewAngle, PI * 2.0f);
+	}
+
+	//視点移動
+	viewProjection_.eye += move;
+	viewProjection_.target += move;
+	//上方向ベクトルを計算
+	viewProjection_.up = { cosf(viewAngle),sinf(viewAngle),0.0f };
+	//行列の再計算
+	viewProjection_.UpdateMatrix();
+
+	//debug
+	debugText_->SetPos(50, 50);
+	debugText_->Printf("eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y
+		, viewProjection_.eye.z);
+	debugText_->SetPos(50, 70);
+	debugText_->Printf("target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y
+		, viewProjection_.target.z);
+	debugText_->SetPos(50, 90);
+	debugText_->Printf("up:(%f,%f,%f)", viewProjection_.up.x, viewProjection_.up.y
+		, viewProjection_.up.z);
 }
 
 void GameScene::Draw() {
@@ -91,7 +153,10 @@ void GameScene::Draw() {
 
 
 	//3Dモデルの描画
-	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	for (WorldTransform& worldTransform : worldTransforms_)
+	{
+		model_->Draw(worldTransform, viewProjection_, textureHandle_);
+	}
 
 	for (int i = -MAXARR; i <= MAXARR; i++)
 	{
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -49,7 +49,7 @@ private: // メンバ変数
 	DebugText* debugText_ = nullptr;
 	uint32_t textureHandle_ = 0;	//テクスチャハンドル
 	Model* model_ = nullptr;		//3Dモデル
-	WorldTransform worldTransform_;
+	WorldTransform worldTransforms_[100];
 	ViewProjection viewProjection_;
 	DebugCamera* debugCamera_ = nullptr;
 
@@ -70,6 +70,8 @@ private: // メンバ変数
 	Vector4 xColor_ = { 1,0,0,0.5 };
 	Vector4 yColor_ = { 0,1,0,0.5 };
 	Vector4 zColor_ = { 0,0,1,0.5 };
+
+	float viewAngle = 0.0f;
 
 	/// <summary>
 	/// ゲームシーン用
